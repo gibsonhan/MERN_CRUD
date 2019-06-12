2f2f4a7add69509ae97dd0dcb9cb85673e617dfc
Gibson Han <gibsonxhan@gmail.com>
2019-06-12 19:33:45 +0700

    removed provider component from redux

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.scss";
 
-import { Provider } from "react-redux";
-import store from "../redux/store";
+//import { Provider } from "react-redux";
+//import store from "../redux/store";
 
 //import Dashboard from "../pages/dashboard/Dashboard";
 import Landing from "../pages/landing/Landing";
@@ -14,16 +14,14 @@ import Register from '../components/auth/Register';
 class App extends Component {
     render() {
         return (
-            <Provider store={store}>
-                <Router>
-                    <div className="App">
-                        <Navbar />
-                        <Route exact path="/" component={Landing} />
-                        <Route exact path="/login" component={Login} />
-                        <Route exact path="/register" component={Register} />
-                    </div>
-                </Router>
-            </Provider>
+            <Router>
+                <div className="App">
+                    <Navbar />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                </div>
+            </Router>
         );
     }
 }
