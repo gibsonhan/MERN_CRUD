2b9e9a0e551e6604c82fe6600cbc6858f873e688
Gibson Han <gibsonxhan@gmail.com>
2019-06-15 02:54:36 +0700

    removing footer from dashboard and creating seperate component

---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Footer.scss";
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <p> Footer Placeholder </p>
+            </div>
+        );
+    }
+}
+
+export default Footer;
