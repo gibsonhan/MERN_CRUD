06801ce333d6b93cff230b4d46da22c764280362
Gibson Han <gibsonxhan@gmail.com>
2019-06-18 04:01:42 +0700

    creating static table to display companyList

--- client/src/pages/company/ListCompany.js
+++ client/src/pages/company/ListCompany.js
@@ -3,7 +3,31 @@ import React, { Component } from 'react';
 class ListCompany extends Component {
     render() {
         return (
-            <p>List Company</p>
+            <div className="container">
+                <div className="listNav">
+                    <p>SearchNav PlaceHolder</p>
+                </div>
+                <div className="companyList-wrapper">
+                    <table className="borded-table">
+                        <thead>
+                            <tr>
+                                <th>Company Name</th>
+                                <th>Last Contacted</th>
+                                <th>Priority</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>RL Electronics</td>
+                                <td>06/18/2019: 10:00AM</td>
+                                <td>4/5</td>
+                                <td><button>Edit</button></td>
+                                <td><button>Archive</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         );
     }
 }
