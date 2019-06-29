18d1922f2ab358311044e925583187eeb8fc8908
Gibson Han <gibsonxhan@gmail.com>
2019-06-28 19:39:23 -0700

    refactor companyTable component to statless react function

--- client/src/pages/company/CompanyTable.js
+++ client/src/pages/company/CompanyTable.js
@@ -1,30 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
 
 import CompanyRow from './CompanyRow';
 
-class CompanyTable extends Component {
-    render() {
-        const companyRows = this.props.companies
-            .map(company => <CompanyRow key={company._id} company={company} />);
-        return (
-            <div>
-                <table className="borded-table">
-                    <thead>
-                        <tr>
-                            <th>Company Name</th>
-                            <th>Contact</th>
-                            <th>Email</th>
-                            <th>Region</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>{companyRows}</tbody>
-                </table>
-                <Link to ="newcompany">Create Company</Link>
-            </div>
-        );
-    }
-}
+const CompanyTable = (props) => {
+    const companyRows = props.companies.map(company =>
+        <CompanyRow key={company._id} company={company} />);
+    return (
+        <div>
+            <table className="borded-table">
+                <thead>
+                    <tr>
+                        <th>Company Name</th>
+                        <th>Contact</th>
+                        <th>Email</th>
+                        <th>Region</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>{companyRows}</tbody>
+            </table>
+            <Link to ="newcompany">Create Company</Link>
+        </div>
+    );
+};
+
+CompanyTable.propTypes = {
+    companies: PropTypes.propTypes
+};
 
 export default CompanyTable;
