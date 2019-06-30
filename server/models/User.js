0e06133dc159cfdbf4a3575208b977df184e8534
Gibson Han <gibsonxhan@gmail.com>
2019-06-29 22:05:27 -0700

    adding isDelete schemal field and bycrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
-
 //Create Schema
 const UserSchema = new Schema({
     name: {
@@ -13,13 +13,27 @@ const UserSchema = new Schema({
     },
     password: {
         type: String,
+        default: '',
         required: true
+    },
+
+    isDeleted: {
+        type: Boolean,
+        default: false
     },
     date: {
         type: Date,
-        default: Date.now
+        default: Date.now ()
     }
 });
 
-module.exports = User = mongoose.model("users", UserSchema);
+UserSchema.methods.generateHash = (password) => {
+    return bcrypt.hashSync(password, bcrypt.genSalt(8), null);
+};
+
+UserSchema.methods.validPassword = (password) => {
+    return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = User = mongoose.model('User', UserSchema);
 
